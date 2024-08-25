d4a12ea7f3f1fed4bbcdf6d67e91611e911ff74f
digmen <digmen68@gmail.com>
2024-08-25 19:08:03 +0600

    norma

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import { AScene, AEntity, ABox, ACamera } from 'aframe-react';
+import { AScene, AEntity, ABox, ACamera, Scene, Entity } from 'aframe-react';
 
 const App = () => {
   return (
-    <AScene>
-      {/* Marker для AR */}
-      <AEntity marker="preset: hiro">
-        {/* Простой 3D-объект: большой куб */}
-        <ABox
-          position="0 0.5 -3"
-          rotation="0 45 0"
-          color="#4CC3D9"
-          depth="2"
-          height="2"
-          width="2"
-        />
-      </AEntity>
-
-      {/* Камера */}
-      <ACamera />
-    </AScene>
+    <Scene>
+      <Entity geometry={{ primitive: 'box' }} material={{ color: 'red' }} position={{ x: 0, y: 0, z: -5 }} />
+      <Entity particle-system={{ preset: 'snow' }} />
+      <Entity light={{ type: 'point' }} />
+      <Entity gltf-model={{ src: 'virtualcity.gltf' }} />
+      <Entity text={{ value: 'Hello, WebVR!' }} />
+    </Scene>
   );
 };
 
