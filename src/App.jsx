ea3a3614860d8394836eeb810370afbebbd354a0
digmen <digmen68@gmail.com>
2024-08-25 19:26:15 +0600

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import 'aframe';
-import 'aframe-ar'; // Импорт AR.js
+import 'aframe-ar'; // Подключение AR.js
 import { Entity, Scene } from 'aframe-react';
 import React from 'react';
 
 const App = () => {
   return (
-    <Scene embedded arjs='sourceType: webcam;'>
-      {/* Камера для AR */}
-      <Entity camera={{ active: true }} look-controls={{ pointerLockEnabled: false }} />
+    <Scene embedded>
+      {/* AR.js для использования камеры */}
+      <a-marker-camera preset="hiro"></a-marker-camera>
 
-      {/* 3D модель куба */}
+      {/* Простая 3D модель (куб) */}
       <Entity
         geometry={{ primitive: 'box' }}
         material={{ color: 'red' }}
-        position={{ x: 0, y: 0, z: -5 }}
+        position={{ x: 0, y: 0, z: -1 }}
       />
 
-      {/* Дополнительные элементы */}
-      <Entity particle-system={{ preset: 'snow' }} />
-      <Entity light={{ type: 'point' }} />
-      <Entity gltf-model={{ src: 'virtualcity.gltf' }} />
-      <Entity text={{ value: 'Hello, AR!' }} />
+      {/* Камера для AR */}
+      <Entity
+        camera
+        look-controls={{ enabled: true }}
+      />
     </Scene>
   );
 };
