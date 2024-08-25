46db59f5b81ac8b2bc28de60b47c5015f5089570
digmen <digmen68@gmail.com>
2024-08-25 19:37:54 +0600

    sssssssssss

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,14 @@ const App = () => {
   return (
     <div className="app-container">
       <Scene embedded>
-        {/* Камера для AR */}
         <a-entity camera position="0 1.6 0" look-controls="pointerLockEnabled: true"></a-entity>
 
-        {/* Куб на фиксированной позиции */}
         <Entity
           geometry={{ primitive: 'box' }}
           material={{ color: 'red' }}
           position={{ x: 0, y: 0, z: -5 }}
         />
 
-        {/* AR.js для использования камеры и отслеживания движения устройства */}
         <a-marker-camera preset="hiro"></a-marker-camera>
       </Scene>
     </div>
