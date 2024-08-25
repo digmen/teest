4994b389366a48e1f58418233bfd1958acc087cc
digmen <digmen68@gmail.com>
2024-08-25 19:43:31 +0600

    sdfsdfsdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import 'aframe';
-import 'aframe-ar'; // Подключение AR.js
+import 'aframe-ar';
 import { Entity, Scene } from 'aframe-react';
 import React from 'react';
-пше
+
 const App = () => {
   return (
     <div className="app-container">
-      <Scene embedded>
-        <a-entity camera position="0 1.6 0" look-controls="pointerLockEnabled: true"></a-entity>
+      <Scene embedded arjs>
+        {/* Камера для AR */}
+        <Entity camera look-controls="pointerLockEnabled: false" />
 
+        {/* Куб на фиксированной позиции */}
         <Entity
           geometry={{ primitive: 'box' }}
           material={{ color: 'red' }}
           position={{ x: 0, y: 0, z: -5 }}
         />
 
-        <a-marker-camera preset="hiro"></a-marker-camera>
+        {/* AR.js для отслеживания маркеров */}
+        <Entity
+          marker={{ preset: 'hiro' }}
+          position={{ x: 0, y: 0, z: 0 }}
+        >
+          <Entity
+            geometry={{ primitive: 'box' }}
+            material={{ color: 'blue' }}
+            position={{ x: 0, y: 0, z: 0 }}
+          />
+        </Entity>
       </Scene>
     </div>
   );
