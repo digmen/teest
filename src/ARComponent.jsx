84a808e8652fe40c857bdfdb536831d1dda4fadb
digmen <digmen68@gmail.com>
2024-08-25 18:51:45 +0600

    sdsdfsdfsdsaasaa

--- src/ARComponent.jsx
+++ src/ARComponent.jsx
@@ -2,24 +2,7 @@ import React from 'react';
 
 const ARComponent = () => {
     return (
-        <a-scene embedded>
-            <a-marker preset="hiro">
-                <a-box
-                    position="0 1 0"
-                    rotation="0 45 0"
-                    color="#4CC3D9"
-                    depth="2"
-                    height="2"
-                    width="2"
-                ></a-box>
-                <a-entity
-                    geometry="primitive: line; vertices: 0 0 0, 0 2 0"
-                    material="color: red; lineWidth: 5"
-                    position="0 0 0"
-                ></a-entity>
-            </a-marker>
-            <a-entity camera></a-entity>
-        </a-scene>
+      
     );
 };
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import ARComponent from './ARComponent';
 
 const App = () => {
   return (
     <>
-      <ARComponent />
+      <a-scene embedded>
+        <a-marker preset="hiro">
+          <a-box
+            position="0 1 0"
+            rotation="0 45 0"
+            color="#4CC3D9"
+            depth="2"
+            height="2"
+            width="2"
+          ></a-box>
+          <a-entity
+            geometry="primitive: line; vertices: 0 0 0, 0 2 0"
+            material="color: red; lineWidth: 5"
+            position="0 0 0"
+          ></a-entity>
+        </a-marker>
+        <a-entity camera></a-entity>
+      </a-scene>
     </>
   );
 };
