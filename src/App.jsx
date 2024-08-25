50927543c3742d8da9c970d3c72d65ad547d6acc
digmen <digmen68@gmail.com>
2024-08-25 18:43:16 +0600

    sdsdfsdfsds

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ const App = () => {
     <a-scene embedded>
       {/* Marker для AR */}
       <a-marker preset="hiro">
-        {/* 3D-модель */}
-        <a-entity
-          gltf-model="url(https://sketchfab.com/3d-models/tube-man-looping-animated-rigged-682bba00f6ec4fef8ee17fb1535d7741)" // Замените на путь к вашей модели
-          scale="0.5 0.5 0.5"
-          position="0 0 0"
-          rotation="0 0 0"
-        ></a-entity>
+        {/* Простой 3D-объект: куб */}
+        <a-box
+          position="0 0.5 0"
+          rotation="0 45 0"
+          color="#4CC3D9"
+          depth="1"
+          height="1"
+          width="1"
+        ></a-box>
       </a-marker>
 
       {/* Камера */}
