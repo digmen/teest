7abc7b4c3c742f2e998152a8ab588fc618e188c3
digmen <digmen68@gmail.com>
2024-08-25 18:48:27 +0600

    sdsdfsdfsdsaas

--- src/ARComponent.jsx
+++ src/ARComponent.jsx
@@ -1,29 +1,23 @@
-// ARComponent.jsx
 import React from 'react';
 
 const ARComponent = () => {
     return (
         <a-scene embedded>
-            {/* Marker для AR */}
             <a-marker preset="hiro">
-                {/* 3D-объект: большой куб */}
                 <a-box
-                    position="0 1 0"  // Поднимите куб немного выше, чтобы он был видим
+                    position="0 1 0"
                     rotation="0 45 0"
                     color="#4CC3D9"
                     depth="2"
                     height="2"
                     width="2"
                 ></a-box>
-                {/* Указатель: линия */}
                 <a-entity
                     geometry="primitive: line; vertices: 0 0 0, 0 2 0"
                     material="color: red; lineWidth: 5"
                     position="0 0 0"
                 ></a-entity>
             </a-marker>
-
-            {/* Камера */}
             <a-entity camera></a-entity>
         </a-scene>
     );
